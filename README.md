3508c15a5776969ef176e1e130427deb6e91115a
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2022-02-17 01:01:47 +0700

    feat: basic API setup onnx and scaler

---
+++ README.md
@@ -0,0 +1,5 @@
+# titanic-service
+
+> Basic Kaggle Tabular Data Problem as a service.
+
+Kaggle notebook can be found [here](https://www.kaggle.com/mkamadeus/titanic-skl2onnx?scriptVersionId=87979236) for insights.
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import joblib
 import onnxruntime as rt
 import numpy as np
 
@@ -7,23 +8,41 @@ app = FastAPI()
 sess = rt.InferenceSession("titanic.onnx")
 
 class Input(BaseModel):
-	Pclass : float
-	SibSp : float
-	Parch : float
-	Age : float
-	Sex_female : float
-	Sex_male : float
-	Embarked_C : float
-	Embarked_Q : float
-	Embarked_S : float
+	pclass : float
+	sibsp : float
+	parch : float
+	age : float
+	sex : str
+	embarked : str
+
+class Output(BaseModel):
+	prediction : float
 
 print(sess.get_inputs()[0])
 print(sess.get_outputs()[0], sess.get_outputs()[1])
 
 val = sess.run([sess.get_outputs()[0].name], {sess.get_inputs()[0].name: np.array([[3, 0, 0, 0.263238, 0, 1, 0, 1, 0]], dtype=np.float32)})[0]
-print(val)
+
+
 @app.post('/')
 async def root(body : Input):
 	input_name = sess.get_inputs()[0].name
 	label_name = sess.get_outputs()[0].name
-	# pred_onx = sess.run([label_name], {input_name: X_test.astype(np.float32)})[0]
+
+	# normal
+	p_1 = np.array([[body.pclass, body.sibsp, body.parch]], dtype=np.float32)
+
+	# scaled
+	scaler = joblib.load('age.scaler')
+	p_2 = scaler.transform(np.array([[body.age]], dtype=np.float32))
+
+	# encoded
+	p_3 = np.array([[1 if body.sex == "M" else 0, 1 if body.sex == "F" else 0]], dtype=np.float32)
+	p_4 = np.array([[1 if body.sex == "C" else 0, 1 if body.sex == "Q" else 0, 1 if body.sex == "S" else 0]], dtype=np.float32)
+	
+	p_final = np.concatenate((p_1, p_2, p_3, p_4), axis=1)
+	
+	prediction = sess.run([label_name], {input_name: p_final})[0]
+	print(p_final)
+
+	return Output(prediction=prediction)
